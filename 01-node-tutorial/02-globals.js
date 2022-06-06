afb4cfe4a198c4890237735a9b52bd4629fa71b6
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-06-05 21:45:00 -0300

    Update 02-globals.js

--- 01-node-tutorial/02-globals.js
+++ 01-node-tutorial/02-globals.js
@@ -1,3 +1,11 @@
+// GLOBALS  - NO WINDOW !!!!
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+console.log(`current paste is => ${__dirname}`);
 console.log(`Initializing file script...`);
 let counter = 1;
 setInterval(() => {
