bc7e3136e2938a3a9369e5e0d31d71d4bf6a6aa5
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-06-05 21:20:02 -0300

    Create 01-intro.js

---
+++ 01-node-tutorial/01-intro.js
@@ -0,0 +1,11 @@
+const amount = 9;
+
+if (amount < 10) {
+  console.log("small than 10");
+} else {
+  console.log("bigger than 10");
+}
+
+console.log(
+  `\n\nhey, its my first node app!!!\nand the amount number is => \t${amount}`
+);
